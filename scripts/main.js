fc5c58d4a45b273f35b386eb5e464a57718561bf
v0vansky <voxelll@hotmail.com>
2022-10-15 23:02:45 +0500

    вторые правки

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ const profileInfo = document.querySelector('.profile__info');
 let profileName = profileInfo.querySelector('.profile__name');
 let profileAbout = profileInfo.querySelector('.profile__about');
 const formElement = overlayEl.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input_name');
-let aboutInput = formElement.querySelector('.popup__input_about');
+let nameInput = formElement.querySelector('.popup__input-name');
+let aboutInput = formElement.querySelector('.popup__input-about');
 
 const toggleOverlay = () => {
   overlayEl.classList.toggle('overlay_opened');
@@ -16,6 +16,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
+  formElement.addEventListener('submit', toggleOverlay);
 }
 
 openProfileEditButton.addEventListener('click', toggleOverlay);
@@ -23,4 +24,3 @@ openProfileEditButton.addEventListener('click', toggleOverlay);
 closePopupButton.addEventListener('click', toggleOverlay);
 
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', toggleOverlay);
