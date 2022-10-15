db52d7587bdfab93fcbaf16eb52804fc02cdf9a2
v0vansky <voxelll@hotmail.com>
2022-10-15 19:41:11 +0500

    первые правки

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,26 @@
 const overlayEl = document.querySelector('.overlay');
 const openProfileEditButton = document.querySelector('.profile__edit-button');
 const closePopupButton = overlayEl.querySelector('.popup__close-button');
-
+const profileInfo = document.querySelector('.profile__info');
+let profileName = profileInfo.querySelector('.profile__name');
+let profileAbout = profileInfo.querySelector('.profile__about');
+const formElement = overlayEl.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__input_name');
+let aboutInput = formElement.querySelector('.popup__input_about');
 
 const toggleOverlay = () => {
   overlayEl.classList.toggle('overlay_opened');
 }
 
-openProfileEditButton.addEventListener('click', () => {
-  toggleOverlay();
-})
-
-closePopupButton.addEventListener('click', () => {
-  toggleOverlay();
-})
-
-
-
-let formElement = overlayEl.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input_name');
-let aboutInput = formElement.querySelector('.popup__input_about');
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileInfo = document.querySelector('.profile__info');
-  let profileName = profileInfo.querySelector('.profile__name');
-  let profileAbout = profileInfo.querySelector('.profile__about');
-  // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-
-  nameInput.value = '';
-  aboutInput.value = '';
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+openProfileEditButton.addEventListener('click', toggleOverlay);
+
+closePopupButton.addEventListener('click', toggleOverlay);
+
 formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', toggleOverlay);
